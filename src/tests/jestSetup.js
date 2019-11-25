95e20547daaeda569cbcd943732fa3c640a18240
André Furquim <andreccomputacao@gmail.com>
2019-11-24 21:55:24 -0300

    :white_check_mark: Add style tests for the <img>

--- src/tests/jestSetup.js
+++ src/tests/jestSetup.js
@@ -1,4 +1,6 @@
 import Adapter from 'enzyme-adapter-react-16';
 import { configure } from 'enzyme';
+import 'jest-styled-components';
 
-configure({ adapter: new Adapter() });
+configure({adapter: new Adapter})
+
